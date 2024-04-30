951a9e1cb3ba068cf029a734a06899f84ee3cf94
lbppool <lbppool@proton.me>
2024-04-30 12:43:13 +0800

    add factory test

--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -7,4 +7,48 @@ contract FactoryTest is ContractTest {
     function setUp() public {
         deploy();
     }
+
+    function test_feeRecipient() public {
+        vm.startPrank(owner);
+        factory.setFeeRecipient(other);
+        vm.stopPrank();
+    }
+
+    function test_platformFee() public {
+        vm.startPrank(owner);
+        factory.setPlatformFee(200);
+        vm.stopPrank();
+    }
+
+    function test_referrerFee() public {
+        vm.startPrank(owner);
+        factory.setReferrerFee(200);
+        vm.stopPrank();
+    }
+
+    function test_swapFee() public {
+        vm.startPrank(owner);
+        factory.setSwapFee(200);
+        vm.stopPrank();
+    }
+
+    function testFail_feeRecipient() public {
+        factory.setFeeRecipient(other);
+    }
+
+    function testFail_platformFee() public {
+        vm.startPrank(user);
+        factory.setPlatformFee(200);
+        vm.stopPrank();
+    }
+
+    function testFail_referrerFee() public {
+        vm.startPrank(owner);
+        factory.setReferrerFee(1001);
+        vm.stopPrank();
+    }
+
+    function testFail_swapFee() public {
+        factory.setSwapFee(200);
+    }
 }
